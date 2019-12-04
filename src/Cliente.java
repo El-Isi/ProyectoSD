273e85446009d0db62a9159b3ad4659a9c0d6470
El-Isi <48951712+El-Isi@users.noreply.github.com>
2019-12-04 16:30:04 -0600

    Actualizando Cliente
    Sobre carga del metodo constructor que permite mandar el mensaje una sola vez

--- src/Cliente.java
+++ src/Cliente.java
@@ -8,31 +8,32 @@ import java.util.Scanner;
 public class Cliente {
     private static boolean[] visitados = new boolean[5];//creo un array de boleanos para saber que servidores ya visite
 
+    //Host del servidor
+    final String[] HOST = {
+            "127.0.0.1",
+            "54.89.184.40",//Luis Angel Vivanco Castellanos
+            "54.221.59.91",//Isidro Fernando Gonzalez Gomez
+            "3.15.8.55", //Luis Angel Duran Garcia
+            "18.206.250.133" //Rubi Elizabeth Ayala Zuñiga
+    }; // Aqui las ips de mis servidores de AWS
+
+    //Puerto del servidor
+    final int PUERTO = 5000;
+
+    //Declaro mis inputs y outputs
+    DataInputStream in;
+    DataOutputStream out;
+    String palabra;
+
     Cliente(int id_server, String usuario) {
-        visitados[id_server] = true;//Siempre el servidor 1° es el primero que visitaremos
-
-        //Host del servidor
-        final String[] HOST = {
-                "127.0.0.1",
-                "127.0.0.1",
-                "127.0.0.1",
-                "127.0.0.1",
-                "127.0.0.1"
-        }; // Aqui las ips de mis servidores de AWS
-
-        //Puerto del servidor
-        final int PUERTO = 5000;
-
-        //Declaro mis inputs y outputs
-        DataInputStream in;
-        DataOutputStream out;
+        visitados[id_server] = true;//Siempre actualizamos el servidor que visitamos
 
         try {
 
             //Pedimos al usuario una palabra y la guardamos en un string
             System.out.println("Introduzca una palabra:");
             Scanner scan = new Scanner(System.in);
-            String palabra = scan.nextLine();
+            palabra = scan.nextLine();
 
             //Creo el socket para conectarme con el cliente
             Socket sc = new Socket(HOST[id_server], PUERTO);
@@ -76,7 +77,61 @@ public class Cliente {
 
                 System.out.println(id);
                 System.out.println("Error al conectarnos al servidor, buscando otro servidor...");
-                new Cliente(id, usuario);
+                new Cliente(id, usuario, palabra);
+            } else {
+                System.out.println("No hay servidores disponibles, intente mas tarde");
+            }
+        }
+    }
+
+    Cliente(int id_server, String usuario, String palabra){
+        visitados[id_server] = true;//Siempre actualizamos el servidor que visitamos
+
+        try {
+
+            //Creo el socket para conectarme con el cliente
+            Socket sc = new Socket(HOST[id_server], PUERTO);
+
+            //Instanciamos nuestras variables de entrada y salida del socket
+            in = new DataInputStream(sc.getInputStream());
+            out = new DataOutputStream(sc.getOutputStream());
+
+            //Envio un mensaje al cliente y el usuario
+            out.writeUTF(palabra);
+            out.writeUTF(usuario);
+
+            //Recibo el mensaje del servidor y lo imprimo
+            String mensaje = in.readUTF();
+            System.out.println(mensaje);
+
+            sc.close(); //Cerramos el socket
+
+        } catch (IOException ex) {
+
+            /* En esta parte para hacerlo a prueba de fallas usamos la falla misma
+            al momento de crearse una excepcion y de manera recursiva volvemos a llamar
+            a nuestra clase Cliente
+             */
+
+            int fin = 0; //Declaramos una variable para nuestra condicion de paro
+
+            //Aqui declaramos un foreach donde por cada servidor visitado incrementaremos nuestra condicion de paro
+            for (boolean visitado : visitados) {
+                if (visitado) fin++;
+            }
+
+            //en esta comparacion si no hemos visitado todos nuestros servidoresque siga intentando conectarse aleatoriamente
+            if (fin < 5) {
+                int id;
+
+                do {
+                    Random random = new Random();
+                    id = (int) Math.floor(random.nextDouble() * 4.0 + 1);
+                } while (visitados[(id)]);
+
+                System.out.println(id);
+                System.out.println("Error al conectarnos al servidor, buscando otro servidor...");
+                new Cliente(id, usuario, palabra);
             } else {
                 System.out.println("No hay servidores disponibles, intente mas tarde");
             }
